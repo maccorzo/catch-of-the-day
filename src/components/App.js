067d38a9c63efb8086a1f8d4403842f03fc07e14
maccorzo <marko@hankkila.me>
2018-03-25 13:55:40 +0300

    after video 13

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = fish => {
+    // take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // add a new fish
+    fishes[`fish-${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({
+      fishes,
+    });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
